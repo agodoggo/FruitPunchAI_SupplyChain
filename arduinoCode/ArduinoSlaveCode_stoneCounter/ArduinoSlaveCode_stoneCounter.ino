d81b1511b022e92070712b3e22b10341bfbd5315
Agoston Walter <agoston.walter@duke.edu>
2020-10-23 18:12:18 +0200

    edited arduino code

--- arduinoCode/ArduinoSlaveCode_stoneCounter/ArduinoSlaveCode_stoneCounter.ino
+++ arduinoCode/ArduinoSlaveCode_stoneCounter/ArduinoSlaveCode_stoneCounter.ino
@@ -12,6 +12,15 @@
 
 #include <Wire.h>
 
+//constants won't change
+const int num_stones = 10; // adjust this for the number of stones
+const int stones[] = {2,3,4,5,6,7,8,9,10,11}; // adjust this for the pins in use
+
+//Variables will change
+int stonesState[num_stones]; 
+
+byte stoneCount = 0;
+
 void setup() {
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onRequest(requestEvent); // register event
@@ -24,6 +33,17 @@ void loop() {
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
 void requestEvent() {
-  Wire.write("hello "); // respond with message of 6 bytes
-  // as expected by master
+  sensorRead();
+  Wire.write(stoneCount); // respond with message of 1 bytes
+  stoneCount=0;
+}
+
+void sensorRead(){
+  for (int j = 0; j < num_stones; j = j + 1){
+    int reading = digitalRead(stones[j]);
+    stonesState[j] = reading;
+  }
+  for (int k = 0; k < num_stones; k = k + 1){
+    stoneCount = stoneCount + stonesState[k];
+  }
 }
