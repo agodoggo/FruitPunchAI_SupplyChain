eadbacc9894272352b912918f9cb34a6cfa0687e
agodoggo <agoston.walter@duke.edu>
2020-10-24 22:30:21 +0200

    commited processing code

--- pythonCode/masterI2C_readData.py
+++ pythonCode/masterI2C_readData.py
@@ -11,13 +11,20 @@ numBoards = 17 #Number of Boards in the game
 boardAddr = [x for x in range (8,8+numBoards)] # board addresses 8 - 25
 boardStoneCount = [0]*17
 with SMBus(1) as bus: # indicates /dev/ic2-1
-   while True:
+   print ("Enter 1 for ON or 0 for OFF")
+   numb = 1
+   while numb == 1:
       boardIndex=0
-      for addr in boardAddr:
-         boardStoneCount[boardIndex] = bus.read_byte_data(addr, 0, 32) # read 32 bytes from address 'addr', offset 0
-         print("Board Number ")
-         print(boardIndex)
-         print(": ")
-         print(boardStoneCount[boardIndex])
-         print("\n")
-         boardIndex = boardIndex+1
+      getData = input(">>>>   ")
+      if getData == 1:
+         for addr in boardAddr:
+            boardStoneCount[boardIndex] = bus.read_byte_data(addr, 0, 32) # read 32 bytes from address 'addr', offset 0
+            print("Board Number ")
+            print(boardIndex)
+            print(": ")
+            print(boardStoneCount[boardIndex])
+            print("\n")
+            boardIndex = boardIndex+1
+      else:
+         numb = 0
+         
