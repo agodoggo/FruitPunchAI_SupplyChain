752a34746357663a6854328742bc534754188eaa
Agoston Walter <agoston.walter@duke.edu>
2021-01-17 06:13:04 -0700

    working on arduino end of LED arrow control

---
+++ arduinoCode/nonSensorSide/nonSensorSide.ino
@@ -0,0 +1,14 @@
+//define all of these
+static int phaseNo = 5
+int phasePins[phaseNo] = {4,7,3,2,6} // {Assembly, Logistics, Transport1, Transport2, Demand}
+int slotCount_dataPin = 5 //make sure to pullup to 20k
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
