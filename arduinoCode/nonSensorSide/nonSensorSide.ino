a659f1a28677a4bdbd67db419c7d584e83b19e9a
Agoston Walter <agoston.walter@duke.edu>
2021-01-19 06:30:01 -0700

    cleaned up nonSensorSide.ino

--- arduinoCode/nonSensorSide/nonSensorSide.ino
+++ arduinoCode/nonSensorSide/nonSensorSide.ino
@@ -2,9 +2,8 @@
 //last modified 18 January 2019
 
 //define all of these
-static int phaseNo = 5
-int phasePins[phaseNo] = {4,7,3,2,6} // {Assembly, Logistics, Transport1, Transport2, Demand}
-int slotCount_dataPin = 5 //make sure to pullup to 20k
+int phasePins[5] = {4,7,3,2,6}; // {Assembly, Logistics, Transport1, Transport2, Demand}
+int slotCount_dataPin = 5; //make sure to pullup to 20k
 char msg_rec;
 char msg_sent;
 
@@ -24,11 +23,11 @@ char DEMAND = '5';
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  for (int i = 0; i < phaseNo; i++){
+  for (int i = 0; i < 5; i++){
     pinMode(phasePins[i],OUTPUT);
   }
   pinMode(slotCount_dataPin, INPUT_PULLUP);
-  establishContact()
+  establishContact();
 }
 
 void loop() {
@@ -38,36 +37,36 @@ void loop() {
 
     // arrow if-else statements
     if (msg_rec == OTHER){
-      for (int i = 0; i < phaseNo; i++){
+      for (int i = 0; i < 5; i++){
         digitalWrite(phasePins[i],LOW);
       }
     }
     else if (msg_rec == ASSEMBLY){
-      for (int i = 0; i < phaseNo; i++){
+      for (int i = 0; i < 5; i++){
         digitalWrite(phasePins[i],LOW);
       }
       digitalWrite(phasePins[0],HIGH);
     }
     else if (msg_rec == LOGISTICS){
-      for (int i = 0; i < phaseNo; i++){
+      for (int i = 0; i < 5; i++){
         digitalWrite(phasePins[i],LOW);
       }
       digitalWrite(phasePins[1],HIGH);
     }
     else if (msg_rec == TRANSPORT1){
-      for (int i = 0; i < phaseNo; i++){
+      for (int i = 0; i < 5; i++){
         digitalWrite(phasePins[i],LOW);
       }
       digitalWrite(phasePins[2],HIGH);
     }
     else if (msg_rec == TRANSPORT2){
-      for (int i = 0; i < phaseNo; i++){
+      for (int i = 0; i < 5; i++){
         digitalWrite(phasePins[i],LOW);
       }
       digitalWrite(phasePins[3],HIGH);
     }
     else if (msg_rec == DEMAND){
-      for (int i = 0; i < phaseNo; i++){
+      for (int i = 0; i < 5; i++){
         digitalWrite(phasePins[i],LOW);
       }
       digitalWrite(phasePins[4],HIGH);
@@ -76,7 +75,7 @@ void loop() {
   }
   else{
     Serial.println("No message");
-    for (int i = 0; i < phaseNo; i++){
+    for (int i = 0; i < 5; i++){
       digitalWrite(phasePins[i],LOW);
     }
 
