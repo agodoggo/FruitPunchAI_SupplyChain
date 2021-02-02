d2b9a8c6ebb3f4ac76d8a1b91f6f6c073463c10e
Agoston Walter <agoston.walter@duke.edu>
2021-02-02 14:57:43 +0100

    Update demandLED_test_nonSensorSide.ino

--- arduinoCode/BoardTests/demandLED_test_nonSensorSide/demandLED_test_nonSensorSide.ino
+++ arduinoCode/BoardTests/demandLED_test_nonSensorSide/demandLED_test_nonSensorSide.ino
@@ -1,17 +1,23 @@
 //demand LED blink test
 const int demand_LED = 6;
+const int LED_COUNT = 16;
+
+Adafruit_NeoPixel strip(LED_COUNT, demand_LED, NEO_GRB+NEOKHZ800)
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(demand_LED,OUTPUT);
+  strip.begin();
+  strip.setBrightness(255);
+  strip.show();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(demand_LED,HIGH);
-  Serial.println("HIGH");
+  for(int i = 0; i < LED_COUNT){
+    strip.setPixelColor(i,255,255,255);
+  }
+  strip.show();
   delay(1000);
-  digitalWrite(demand_LED,LOW);
-  Serial.println("LOW");
+  strip.clear();
   delay(1000);
 }
