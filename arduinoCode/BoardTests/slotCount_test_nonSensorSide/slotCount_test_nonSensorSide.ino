6ae60a9c7a9904e7b835b5d83410ec9c0f9bacd9
Agoston Walter <agoston.walter@duke.edu>
2021-02-02 18:27:00 +0100

    Update slotCount_test_nonSensorSide.ino

--- arduinoCode/BoardTests/slotCount_test_nonSensorSide/slotCount_test_nonSensorSide.ino
+++ arduinoCode/BoardTests/slotCount_test_nonSensorSide/slotCount_test_nonSensorSide.ino
@@ -1,6 +1,7 @@
 //demand LED blink test
 const int slotCount = A5;
 int toggle = HIGH;
+int count = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -10,10 +11,16 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   int sensValue = digitalRead(slotCount);
-  if (sensValue != toggle){
-    toggle = sensValue;
-    Serial.println(millis());
+  if (sensValue == LOW){
+    count++;
+    delay(100); //to give time for stone to pass through, no multiple readings
+    Serial.println(count);
   }
+  
+//  if (sensValue != toggle){
+//    toggle = sensValue;
+//    Serial.println(millis());
+//  }
 //  float slotCount = sensValue * (5.0/1023.0);
 //  Serial.println(sensValue);
 }
