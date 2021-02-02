54bb080a0a2c8c295a918309d51773adef32704f
Agoston Walter <agoston.walter@duke.edu>
2021-02-02 18:21:00 +0100

    Update slotCount_test_nonSensorSide.ino

--- arduinoCode/BoardTests/slotCount_test_nonSensorSide/slotCount_test_nonSensorSide.ino
+++ arduinoCode/BoardTests/slotCount_test_nonSensorSide/slotCount_test_nonSensorSide.ino
@@ -11,7 +11,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   int sensValue = digitalRead(slotCount);
   if (sensValue != toggle){
-    Serial.println(mills());
+    Serial.println(millis());
   }
 //  float slotCount = sensValue * (5.0/1023.0);
 //  Serial.println(sensValue);
