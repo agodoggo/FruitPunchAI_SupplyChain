cfc9da5bcdc968976067278ec71f12596c3ad1ba
Agoston Walter <agoston.walter@duke.edu>
2021-02-02 14:43:12 +0100

    Update demandLED_test_nonSensorSide.ino

--- arduinoCode/BoardTests/demandLED_test_nonSensorSide/demandLED_test_nonSensorSide.ino
+++ arduinoCode/BoardTests/demandLED_test_nonSensorSide/demandLED_test_nonSensorSide.ino
@@ -1,14 +1,17 @@
 //demand LED blink test
-const int demand_LED = 5;
+const int demand_LED = 6;
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(demand_LED,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(demand_LED,HIGH);
+  Serial.println("HIGH");
   delay(1000);
   digitalWrite(demand_LED,LOW);
+  Serial.println("LOW");
   delay(1000);
 }
