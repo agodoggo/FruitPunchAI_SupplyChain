cae6ac011952381b8cc71a3e1de07e697e55d638
Agoston Walter <agoston.walter@duke.edu>
2021-02-02 15:04:17 +0100

    Update demandLED_test_nonSensorSide.ino

--- arduinoCode/BoardTests/demandLED_test_nonSensorSide/demandLED_test_nonSensorSide.ino
+++ arduinoCode/BoardTests/demandLED_test_nonSensorSide/demandLED_test_nonSensorSide.ino
@@ -4,7 +4,7 @@
 const int demand_LED = 6;
 const int LED_COUNT = 16;
 
-Adafruit_NeoPixel strip(LED_COUNT, demand_LED, NEO_GRB+NEO_KHZ800)
+Adafruit_NeoPixel strip(LED_COUNT, demand_LED, NEO_GRB+NEO_KHZ800);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -15,7 +15,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int i = 0; i < LED_COUNT){
+  for(int i = 0; i < LED_COUNT; i++){
     strip.setPixelColor(i,255,255,255);
   }
   strip.show();
