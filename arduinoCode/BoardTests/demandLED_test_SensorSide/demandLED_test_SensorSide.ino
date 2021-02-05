250d625f887e6caa50b0f54da9994ce0315bb7c0
Agoston Walter <agoston.walter@duke.edu>
2021-02-05 18:08:27 +0100

    Create demandLED_test_SensorSide.ino

---
+++ arduinoCode/BoardTests/demandLED_test_SensorSide/demandLED_test_SensorSide.ino
@@ -0,0 +1,34 @@
+#include <Adafruit_NeoPixel.h>
+
+//demand LED blink test
+const int demand_LED = 2;
+const int LED_COUNT = 16;
+
+Adafruit_NeoPixel strip(LED_COUNT, demand_LED, NEO_GRB+NEO_KHZ800);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  strip.begin();
+  strip.setBrightness(120);
+  strip.show();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  setLEDStripHIGH();
+  delay(1000);
+  setLEDStripLOW();
+  delay(1000);
+}
+
+void setLEDStripHIGH(){
+  for(int i = 0; i < LED_COUNT; i++){
+    strip.setPixelColor(i,255,255,255);
+  }
+  strip.show();
+}
+
+void setLEDStripLOW(){
+  strip.clear();
+  strip.show();
+}
