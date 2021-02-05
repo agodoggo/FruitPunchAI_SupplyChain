7ac0c1868315a875d3b7c076806ad5a4c1c948ef
Agoston Walter <agoston.walter@duke.edu>
2021-02-05 17:47:59 +0100

    Update analogTester2.ino

--- arduinoCode/miscTests/analogTester2/analogTester2.ino
+++ arduinoCode/miscTests/analogTester2/analogTester2.ino
@@ -1,10 +1,10 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(A14,INPUT);
+  pinMode(A15,INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(analogRead(A14)*(5.0)/(1023.0));
+  Serial.println(analogRead(A15)*(5.0)/(1023.0));
 }
