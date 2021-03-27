70e0439cf28aae8b5651ed5edd5317ef51d1dabb
Agoston Walter <43449713+agodoggo@users.noreply.github.com>
2021-03-27 14:44:05 +0100

    Update SCGamePredictor.keras.py

--- AI/model.4.0/SCGamePredictor.keras.py
+++ AI/model.4.0/SCGamePredictor.keras.py
@@ -239,7 +239,7 @@ print(state)
 recommendation = keras_model.get_recommendation(state)
 print(recommendation)
 filePath = sys.argv[2]
-writeFile(filePath,state)
+keras_model.writeFile(filePath,state)
 
 # keras_model.play()
 
